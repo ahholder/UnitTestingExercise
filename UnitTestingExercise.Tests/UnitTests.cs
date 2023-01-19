471228dcf7fdfb29ca166933b8d13f71809b79c6
Alexander Holder <drowish95z@gmail.com>
2023-01-19 10:57:47 -0600

    Submission #1 (4/4)

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -13,53 +13,64 @@ namespace UnitTestingExercise.Tests
 
             //Arrange
             // create a Calculator object
+            var cal = new Calculator();
             
 
             //Act
                 // call the Add method that is located in the Calculator class
                 // and store its result in a variable named actual
+                var actual = cal.Add(num1, num2, num3);
 
             //Assert
-                //Assert.Equal(expected, actual);
+                Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(10,8,2)]//Add test data <-------
         public void SubtractTest(int minuend, int subtrhend, int expected)
         {
             //Start Step 5 here:
 
             //Arrange
+            var cal = new Calculator();
 
             //Act
+            var actual = cal.Subtract(minuend, subtrhend);
 
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(3,5,15)]//Add test data <-------
         public void MultiplyTest(int num1, int num2, int expected)
         {
             //Start Step 7 here:
 
             //Arrange
+            var cal = new Calculator();
 
             //Act
+            var actual = cal.Multiply(num1, num2);
 
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(10,2,5)]//Add test data <-------
         public void DivideTest(int num1, int num2, int expected)
         {
             //Arrange
+            var cal = new Calculator();
 
             //Act
+            var actual = cal.Divide(num1, num2);
 
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
--- UnitTestingExercise/Program.cs
+++ UnitTestingExercise/Program.cs
@@ -6,7 +6,7 @@ namespace UnitTestingExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test Explorer: 4/4 Passed!");
         }
     }
 }
